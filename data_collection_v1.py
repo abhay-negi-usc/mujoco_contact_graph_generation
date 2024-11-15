17fdf9222b556852e7d44479fddb78a26b8668b0
abhay-negi-usc <abhay.negi@usc.edu>
2024-11-15 12:28:27 -0800

    attempting to merge, also added gitignore for results dir

--- data_collection_v1.py
+++ data_collection_v1.py
@@ -11,11 +11,13 @@ import cv2
 xml_path = "./env/key_env_v2.xml" 
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
-num_trials = 1000 
-dir_results = "./results/data_v2" 
-os.makedirs(dir_results + "/pkl", exist_ok=True)
-os.makedirs(dir_results + "/vid", exist_ok=True)
+num_trials = 50000 # 1k points ~ 27 minutes with videos, 30 seconds without videos 
+dir_results = "./results/data_v4" 
 flag_show_video = False 
+flag_save_video = False 
+os.makedirs(dir_results + "/pkl", exist_ok=True)
+if flag_save_video: 
+    os.makedirs(dir_results + "/vid", exist_ok=True)
 
 n_frames = 500  
 height = 720 
@@ -37,94 +39,98 @@ model.vis.map.force = 0.3
 
 for idx_trial in range(num_trials): 
 
-  # define initial conditions 
-  x0 = 2*(np.random.rand() - 0.5) * 1e-3 * 5.0
-  y0 = 2*(np.random.rand() - 0.5) * 1e-3 * 5.0
-  z0 = 2*(np.random.rand() - 0.5) * 1e-3 * 1.0 
-  a0 = 2*(np.random.rand() - 0.5) * (np.pi/180) * 3.0 
-  b0 = 2*(np.random.rand() - 0.5) * (np.pi/180) * 3.0 
-  c0 = 2*(np.random.rand() - 0.5) * (np.pi/180) * 3.0 
+    # define initial conditions 
+    x0 = 2*(np.random.rand() - 0.5) * 1e-3 * 1.0 
+    y0 = 2*(np.random.rand() - 0.5) * 1e-3 * 1.0 
+    z0 = 2*(np.random.rand() - 0.5) * 1e-3 * 1.0 - 5e-3 
+    a0 = 2*(np.random.rand() - 0.5) * (np.pi/180) * 5.0 
+    b0 = 2*(np.random.rand() - 0.5) * (np.pi/180) * 5.0 
+    c0 = 2*(np.random.rand() - 0.5) * (np.pi/180) * 5.0 
 
-  mujoco.mj_resetData(model, data)
-  data.qpos = np.array([x0, y0, z0, a0, b0, c0]) 
-  data.qvel = np.zeros(6) 
-  mujoco.mj_forward(model, data)
+    mujoco.mj_resetData(model, data)
+    data.qpos = np.array([x0, y0, z0, a0, b0, c0]) 
+    data.qvel = np.zeros(6) 
+    mujoco.mj_forward(model, data)
 
-  # initialize data structures 
-  frames = []
-  state_hist = np.zeros((n_frames,1+3+4))   
-  contact_hist = [] 
-  contact_num = [] 
-  contact_geom1 = [] 
-  contact_geom2 = [] 
-  contact_dist = [] 
-  contact_pos = [] 
-  contact_frame = [] 
-  ctrl_hist = np.zeros((n_frames,1+6))   
-  sensor_hist = np.zeros((n_frames,13))
+    # initialize data structures 
+    frames = []
+    state_hist = np.zeros((n_frames,1+3+4))   
+    contact_hist = [] 
+    contact_num = [] 
+    contact_geom1 = [] 
+    contact_geom2 = [] 
+    contact_dist = [] 
+    contact_pos = [] 
+    contact_frame = [] 
+    ctrl_hist = np.zeros((n_frames,1+6))   
+    sensor_hist = np.zeros((n_frames,13))
 
 
-  # define controller parameters  
-  qpos_insert = np.array([0, 0, -10e-3, 0, 0, 0]) 
-  z_step = 1e-3 
+    # define controller parameters  
+    qpos_insert = np.array([0, 0, -10e-3, 0, 0, 0]) 
+    z_step = 1e-3 
 
-  # Initialize video writer
-  video_path = dir_results + f"/vid/trial_{idx_trial}.mp4"
-  fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-  video_writer = cv2.VideoWriter(video_path, fourcc, 30, (width, height))
+    if flag_save_video: 
+        # Initialize video writer
+        video_path = dir_results + f"/vid/trial_{idx_trial}.mp4"
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+        video_writer = cv2.VideoWriter(video_path, fourcc, 30, (width, height))
 
-  # Simulate and display video.
-  with mujoco.Renderer(model, height, width) as renderer:
-    for i in range(n_frames): 
-      while data.time < i/(30.0*4): #1/4x real time
-        mujoco.mj_step(model, data)
-      renderer.update_scene(data, "track", options)
-      frame = renderer.render()
-      frames.append(frame)
+    # Simulate and display video.
+    with mujoco.Renderer(model, height, width) as renderer:
+        for i in range(n_frames): 
+            while data.time < i/(30.0*4): #1/4x real time
+                mujoco.mj_step(model, data)
+            if flag_show_video or flag_save_video: 
+                renderer.update_scene(data, "track", options)
+                frame = renderer.render()
+                frames.append(frame)
 
-      # Convert frame to BGR format for OpenCV
-      frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-      video_writer.write(frame_bgr)
+                # Convert frame to BGR format for OpenCV
+                if flag_save_video:
+                    frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                    video_writer.write(frame_bgr)
 
-      # save data 
-      state_hist[i,:] = np.concatenate([np.array([data.time]), data.xpos[2], data.xquat[2]]) 
-      contact_num.append(len(data.contact.geom1)) 
-      contact_geom1.append(np.array(data.contact.geom1)) 
-      contact_geom2.append(np.array(data.contact.geom2)) 
-      contact_dist.append(np.array(data.contact.dist))  
-      contact_pos.append(np.array(data.contact.pos)) 
-      contact_frame.append(np.array(data.contact.frame)) 
-      ctrl_hist[i,:] = np.concatenate([np.array([data.time]), data.ctrl])  
-      sensor_hist[i,:] = data.sensordata  
+            # save data 
+            state_hist[i,:] = np.concatenate([np.array([data.time]), data.xpos[2], data.xquat[2]]) 
+            contact_num.append(len(data.contact.geom1)) 
+            contact_geom1.append(np.array(data.contact.geom1)) 
+            contact_geom2.append(np.array(data.contact.geom2)) 
+            contact_dist.append(np.array(data.contact.dist))  
+            contact_pos.append(np.array(data.contact.pos)) 
+            contact_frame.append(np.array(data.contact.frame)) 
+            ctrl_hist[i,:] = np.concatenate([np.array([data.time]), data.ctrl])  
+            sensor_hist[i,:] = data.sensordata  
 
-      # controller update 
-      data.ctrl = data.qpos 
-      peg_z_axis = data.xmat[2].reshape(3,3)[:,2]  
-      if i > 10: 
-        data.ctrl = data.qpos - np.concatenate([peg_z_axis*z_step, np.zeros(3)]) # push in tool -z direction 
+            # controller update 
+            data.ctrl = data.qpos 
+            peg_z_axis = data.xmat[2].reshape(3,3)[:,2]  
+            if i > 10: 
+                data.ctrl = data.qpos - np.concatenate([peg_z_axis*z_step, np.zeros(3)]) # push in tool -z direction 
 
-  if flag_show_video: 
-    media.show_video(frames, fps=30)
+        if flag_show_video: 
+            media.show_video(frames, fps=30)
 
-  # Release video writer
-  video_writer.release() 
+        if flag_save_video:
+            # Release video writer
+            video_writer.release() 
 
-#   df_state = pd.DataFrame(state_hist, columns=['t','x','y','z','qw','qx','qy','qz']) 
+    #   df_state = pd.DataFrame(state_hist, columns=['t','x','y','z','qw','qx','qy','qz']) 
 
-  data_dict = {
-    'state_hist': state_hist, 
-    'contact_num': contact_num,   
-    'contact_geom1': contact_geom1,
-    'contact_geom2': contact_geom2,
-    'contact_dist': contact_dist,
-    'contact_pos': contact_pos,
-    'contact_frame': contact_frame,
-    'ctrl_hist': ctrl_hist,
-    'sensor_hist': sensor_hist,
-  }
+        data_dict = {
+            'state_hist': state_hist, 
+            'contact_num': contact_num,   
+            'contact_geom1': contact_geom1,
+            'contact_geom2': contact_geom2,
+            'contact_dist': contact_dist,
+            'contact_pos': contact_pos,
+            'contact_frame': contact_frame,
+            'ctrl_hist': ctrl_hist,
+            'sensor_hist': sensor_hist,
+        }
 
-  # save data as pkl file 
-  with open(dir_results + f"/pkl/trial_{idx_trial}.pkl", 'wb') as f: 
-    pickle.dump(data_dict, f) 
-  
-  print(f"Trial {idx_trial} complete.") 
+        # save data as pkl file 
+        with open(dir_results + f"/pkl/trial_{idx_trial}.pkl", 'wb') as f: 
+            pickle.dump(data_dict, f) 
+        
+        print(f"Trial {idx_trial} complete.") 
